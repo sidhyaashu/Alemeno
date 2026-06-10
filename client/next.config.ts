30f39341ca2b4796edda7bc7f9c0da216a892bb6
sidhyaashu <ashutoshsidhya69@gmail.com>
2026-06-11 03:43:10 +0530

    X

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["graceful-venue-refute.ngrok-free.dev"],
 };
 
 export default nextConfig;
